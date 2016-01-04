db557ffeb61d93c064fbec2579e6213af7cc2cd6
Chad Engler <englercj@live.com>
2016-01-03 21:21:04 -0800

    fix site js

---
+++ js/site.js
@@ -0,0 +1,24 @@
+/**
+ * General site js file.
+ */
+(function ($) {
+    'use strict';
+
+    $(function () {
+        var $emptyHeaders = $('.blog-no-header').parents('.post-header');
+        $emptyHeaders.parents('.post-item').addClass('no-header');
+        $emptyHeaders.remove();
+
+        var $contactwrapper = $('#contact-wrapper');
+
+        $('#contact-section').on('click', function (e) {
+            e.preventDefault();
+            $contactwrapper.animate({ top:0 }, 300);
+        });
+
+        $('#contact-close').on('click', function (e) {
+            e.preventDefault();
+            $contactwrapper.animate({ top:'100%' }, 300);
+        });
+    });
+})(jQuery);
